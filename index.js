7dbe8813f11bd9f23cd9226ab139b63c17e367e9
mattmck <matthew.mckerroll@gmail.com>
2020-12-03 13:34:55 -0500

    started basic structure for readme, as well as questions.

--- index.js
+++ index.js
@@ -1,16 +1,75 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require('./generateMarkdown.js');
+
+
 // array of questions for user
 const questions = [
+    {
+        name: "username",
+        message: "What is your GitHub username?",
 
+    },
+    {   
+        name: "title",
+        message: "Enter your project title:",
+    },
+    {   
+        name: "description", 
+        message: "Enter a project description:",
+    },
+    {
+        name: "installation",
+        message: "Enter installation instructions:"
+    },
+    {
+        name: "usage",
+        message: "Enter usage informaton:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Select license type:",
+        choices: ["copyleft","lpgl","MIT","permissive","proprietary","public"]
+    },
+    {
+        name: "contributors",
+        message: "Contribution Guidelines:"
+    },
+    {
+        name: "tests",
+        message: "Enter test instructions:"
+    },
+    {
+        name: "email",
+        message: "Enter contact email:",
+    }
 ];
-
-// function to write README file
+/* // function to write README file
 function writeToFile(fileName, data) {
-}
+} */
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then(resp => {
 
+        generateReadMe(resp);
+    });
 }
 
+function generateReadMe(responses){
+
+    fs.writeFile
+    (
+        "./output/README.md",
+        generateMarkdown.getReadMe(responses),
+        (err) => {
+            if(err)
+                console.log("An error occured while writing file");
+            else
+                console.log("File saved");
+        }
+    );
+}
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,27 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
+function getReadme(data) {
   return `# ${data.title}
+
+  ${data.description}
+
+  ### Contents
+1. [Installation Instructions](#installation-instructions)
+2. [Usage](#usage)
+3. [Contributors](#contributors)
+4. [Tests](#tests)
+5. [Questions](#questions)
+
+# Installation Instructions
+
+# Usage
+
+# Contributors
+
+# Tests
+
+# Questions
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = getReadme;
